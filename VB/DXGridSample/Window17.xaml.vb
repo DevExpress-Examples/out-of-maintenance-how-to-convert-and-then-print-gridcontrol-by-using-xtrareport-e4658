9c758c08fc884f454544f2c76d59332e2314f431
DevExpressExampleBot <fakeEmail>
2022-12-08 15:12:23 +0400

    Source auto update [skip ci]

--- VB/DXGridSample/Window17.xaml.vb
+++ VB/DXGridSample/Window17.xaml.vb
@@ -1,28 +1,30 @@
-﻿Imports System
-Imports System.Windows
-Imports System.Collections.ObjectModel
 Imports DevExpress.Xpf.Printing
 Imports DevExpress.XtraReports.UI
+Imports System.Collections.ObjectModel
+Imports System.Windows
 
 Namespace DevExpressGrid_Flashing
-    Partial Public Class Window17
+
+    Public Partial Class Window17
         Inherits Window
 
         Private coll As ObservableCollection(Of MyObj)
+
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
             coll = New ObservableCollection(Of MyObj)()
             coll.Add(New MyObj() With {.Text = "n0", .Number = 1, .Group = "A", .MyDateTime = Date.Now})
             coll.Add(New MyObj() With {.Text = "n1", .Number = 123456789.12, .Group = "A", .MyDateTime = Date.Now})
             coll.Add(New MyObj() With {.Text = "n2", .Number = 123456.78, .Group = "B", .MyBool = True, .MyDateTime = Date.Now})
             coll.Add(New MyObj() With {.Text = "n3", .Number = 1245.56, .Group = "B", .MyBool = False, .MyDateTime = Date.Now})
-            grid.ItemsSource = coll
-            grid.View.FocusedRowHandle = 2
+            Me.grid.ItemsSource = coll
+            Me.grid.View.FocusedRowHandle = 2
         End Sub
+
         Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim helper As New ReportHelper.ReportGeneratonHelper()
-            Dim report As XtraReport = helper.GenerateReport(grid, grid.ItemsSource)
-            Dim dp As New DocumentPreviewWindow() With {.Owner = Me}
+            Dim helper As ReportHelper.ReportGeneratonHelper = New ReportHelper.ReportGeneratonHelper()
+            Dim report As XtraReport = helper.GenerateReport(Me.grid, Me.grid.ItemsSource)
+            Dim dp As DocumentPreviewWindow = New DocumentPreviewWindow() With {.Owner = Me}
             dp.PreviewControl.DocumentSource = report
             report.CreateDocument(False)
             dp.ShowDialog()
@@ -30,11 +32,17 @@ Namespace DevExpressGrid_Flashing
     End Class
 
     Public Class MyObj
-        Public Property MyBool() As Boolean
-        Public Property Text() As String
-        Public Property Number() As Double
-        Public Property Group() As String
-        Public Property MyDateTime() As Date
-        Public Property MyTime() As Date
+
+        Public Property MyBool As Boolean
+
+        Public Property Text As String
+
+        Public Property Number As Double
+
+        Public Property Group As String
+
+        Public Property MyDateTime As Date
+
+        Public Property MyTime As Date
     End Class
 End Namespace
--- VB/DXGridSample/XtraReport1.vb
+++ VB/DXGridSample/XtraReport1.vb
@@ -1,16 +1,10 @@
-﻿Imports System
-Imports System.Drawing
-Imports System.Collections
-Imports System.ComponentModel
-Imports DevExpress.XtraReports.UI
-
 Namespace DXGrid_AssignComboBoxToColumn
-    Partial Public Class XtraReport1
+
+    Public Partial Class XtraReport1
         Inherits DevExpress.XtraReports.UI.XtraReport
 
         Public Sub New()
             InitializeComponent()
         End Sub
-
     End Class
 End Namespace
