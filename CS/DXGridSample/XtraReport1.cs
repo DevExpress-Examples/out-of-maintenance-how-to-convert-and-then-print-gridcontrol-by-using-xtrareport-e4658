72f0bf81c4a6782a18d707158c4db4898a058279
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-16 17:37:37 +0400

    Change example status to out of maintenance

--- CS/DXGridSample/XtraReport1.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DXGrid_AssignComboBoxToColumn {
-    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
-        public XtraReport1() {
-            InitializeComponent();
-        }
-
-    }
-}
